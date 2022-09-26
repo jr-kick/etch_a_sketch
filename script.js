43af75bc3576fa718c415b1085b48b0d24f678d2
jr-kick <kickandris@gmail.com>
2022-09-26 21:02:26 +0200

    Created the sketch grid Created a sketch pad with slider to resize

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-const = container 
+const container = document.getElementById('container');
+const slider = document.getElementById('slider');
+
+slider.addEventListener('click', resize);
+
+function resize() {
+    clearContainer();
+    setUpContainer();
+};
+
+function clearContainer() {
+    container.innerHTML = '';
+};
+
+function setUpContainer() {
+
+    const squares = slider.value;
+
+    for (let i = 0; i < squares * squares; i++) {
+        const div = document.createElement('div');
+        container.appendChild(div);
+    };
+
+    container.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
+};
+
